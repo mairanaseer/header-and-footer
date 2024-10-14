8b76d89516261ba5d1bacdf0060db39db5d9cb50
maira naseer ud din <161260376+mairanaseer@users.noreply.github.com>
2024-10-14 23:46:37 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,14 @@
+ export default function header (){
+    return(
+       <div>
+        <h1><b>HEADER</b></h1>
+        <ol>
+            <li>mobile</li>
+            <li>handfree</li>
+            <li>charger</li>
+            <li>earpods</li>
+            <li>back-cover</li>
+        </ol>
+       </div>
+    )
+}
